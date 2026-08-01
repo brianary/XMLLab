eb085308dbfc9364a848ac0d5369ff6a6bc50f34
Brian Lalonde <brianary@gmail.com>
2026-08-01 12:42:17 -0700

    ✅ Add tests for Merge-XmlSelections
    - resolves #13

--- test/Merge-XmlSelections.Tests.ps1
+++ test/Merge-XmlSelections.Tests.ps1
@@ -1,33 +1,24 @@
 ﻿<#
 .SYNOPSIS
-Tests Builds an object using the named XPath selections as properties.
+Tests building an object using the named XPath selections as properties.
 #>
 
-return #TODO: finish tests
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
 	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
+	$datadir = Join-Path $PSScriptRoot 'data'
 }
 Describe 'Merge-XmlSelections' -Tag Merge-XmlSelections,Merge,XmlSelections {
-	Context 'Builds an object using the named XPath selections as properties.' -Tag Example {
-		It "EXAMPLE 1" -Skip {
-			Merge-XmlSelections @{Version='/*/@version';Format='/xsl:output/@method'} *.xsl* -Namespace @{xsl='http://www.w3.org/1999/XSL/Transform'} |Should -BeExactly @"
-Path                    Version Format
-----                    ------- ------
-Z:\Scripts\dataref.xslt 2.0     html
-Z:\Scripts\xhtml2fo.xsl 1.0     xml
-"@
-		}
-	}
-	Context 'Xml' -Tag Xml {
-		It "test" -Skip {
-			1 |Should -Be 1
-		}
-	}
-	Context 'Path' -Tag Path {
-		It "test" -Skip {
-			1 |Should -Be 1
+	Context 'Builds an object using the named XPath selections as properties.' {
+		It "Performs multiple selections and returns them as properties" {
+			$result = Merge-XmlSelections @{
+				Version = '/*/@version'
+				Format  = '/xsl:transform/xsl:output/@method'
+			} "$datadir/xslt-test.xslt" -Namespace @{xsl='http://www.w3.org/1999/XSL/Transform'}
+			$result.Path |Should -BeLike '*xslt-test.xslt'
+			$result.Version |Should -Be '1.0'
+			$result.Format |Should -Be 'text'
 		}
 	}
 }
