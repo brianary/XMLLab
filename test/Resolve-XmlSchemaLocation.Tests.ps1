1ea01ef22496af62d1c736c183b8e06ccad437ea
Brian Lalonde <brianary@gmail.com>
2026-08-02 12:08:24 -0700

    ﻿✅ Add test for Resolve-XmlSchemaLocation
    resolves #15

--- test/Resolve-XmlSchemaLocation.Tests.ps1
+++ test/Resolve-XmlSchemaLocation.Tests.ps1
@@ -1,40 +1,40 @@
 ﻿<#
 .SYNOPSIS
-Tests Gets the namespaces and their URIs and URLs from a document.
+Tests getting the namespaces and their URIs and URLs from a document.
 #>
 
-return #TODO: finish tests
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
 	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
+	$datadir = Join-Path $PSScriptRoot 'data'
 }
 Describe 'Resolve-XmlSchemaLocation' -Tag Resolve-XmlSchemaLocation,Resolve,XmlSchemaLocation {
-	Context 'Gets the namespaces and their URIs and URLs from a document.' -Tag Example {
-		It "EXAMPLE 1" -Skip {
-			Resolve-XmlSchemaLocation test.xml |Should -BeExactly @"
-Path  : C:\test.xml
-Node  : root
-Alias : xml
-Urn   : http://www.w3.org/XML/1998/namespace
-Url   :
-
-Path  : C:\test.xml
-Node  : root
-Alias : xsi
-Urn   : http://www.w3.org/2001/XMLSchema-instance
-Url   :
-"@
-		}
-	}
-	Context 'Xml' -Tag Xml {
-		It "test" -Skip {
-			1 |Should -Be 1
-		}
-	}
-	Context 'Path' -Tag Path {
-		It "test" -Skip {
-			1 |Should -Be 1
+	Context 'Gets the namespaces and their URIs and URLs from a document.' {
+		It "Finds the schemata within the given XML document namespaces" {
+			$schemata = @{}
+			Resolve-XmlSchemaLocation (Join-Path $datadir xslt-schemata.xslt) |
+				ForEach-Object {$schemata[$_.Alias] = $_}
+			$schemata['xml'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata['xml'].Alias |Should -BeExactly xml
+			$schemata['xml'].Urn |Should -BeExactly http://www.w3.org/XML/1998/namespace
+			$schemata['xml'].Url |Should -BeNullOrEmpty
+			$schemata['xsi'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata['xsi'].Alias |Should -BeExactly xsi
+			$schemata['xsi'].Urn |Should -BeExactly http://www.w3.org/2001/XMLSchema-instance
+			$schemata['xsi'].Url |Should -BeNullOrEmpty
+			$schemata['fn'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata['fn'].Alias |Should -BeExactly fn
+			$schemata['fn'].Urn |Should -BeExactly http://www.w3.org/2005/xpath-functions
+			$schemata['fn'].Url |Should -BeNullOrEmpty
+			$schemata['xsl'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata['xsl'].Alias |Should -BeExactly xsl
+			$schemata['xsl'].Urn |Should -BeExactly http://www.w3.org/1999/XSL/Transform
+			$schemata['xsl'].Url |Should -BeExactly http://www.w3.org/1999/XSL/Transform.xsd
+			$schemata[''].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata[''].Alias |Should -BeNullOrEmpty
+			$schemata[''].Urn |Should -BeExactly http://www.w3.org/1999/xhtml
+			$schemata[''].Url |Should -BeExactly http://www.w3.org/1999/xhtml.xsd
 		}
 	}
 }
