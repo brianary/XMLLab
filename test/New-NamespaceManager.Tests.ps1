163cbb03252c89dd917f90dd50ece1bc3314f40b
Brian Lalonde <brianary@gmail.com>
2026-08-01 21:30:23 -0700

    ✅ Add tests for New-NamespaceManager
    resolves #14

--- test/New-NamespaceManager.Tests.ps1
+++ test/New-NamespaceManager.Tests.ps1
@@ -1,21 +1,23 @@
 ﻿<#
 .SYNOPSIS
-Tests Creates an object to lookup XML namespace prefixes.
+Tests criating an object to lookup XML namespace prefixes.
 #>
 
-return #TODO: finish tests
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
 	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
 }
 Describe 'New-NamespaceManager' -Tag New-NamespaceManager,New,NamespaceManager {
-	Context 'Creates an object to lookup XML namespace prefixes.' -Tag Example {
-		It "EXAMPLE 1" -Skip {
-			$n = New-NamespaceManager; (Select-Xml //xhtml:td dataref.xslt).Node.SelectSingleNode('xhtml:var',$n).OuterXml |Should -BeExactly @"
-<var xmlns="http://www.w3.org/1999/xhtml">ANY</var>
-<var xmlns="http://www.w3.org/1999/xhtml">ANY</var>
-"@
+	Context 'Creates an object to lookup XML namespace prefixes.' {
+		It "Returns a new XmlNamespaceManager" {
+			$n = New-NamespaceManager @{xsl='http://www.w3.org/1999/XSL/Transform'}
+			,$n |Should -BeOfType System.Xml.XmlNamespaceManager -Because 'The correct type should be returned'
+			$n.LookupNamespace('xsl') |Should -BeExactly http://www.w3.org/1999/XSL/Transform `
+				-Because 'The namespace URL should be returned given the prefix'
+			$n.LookupPrefix('http://www.w3.org/1999/XSL/Transform') |Should -BeExactly xsl `
+				-Because 'The prefix should be returned given the namespace URL'
+			$n.LookupNamespace('x') |Should -BeNullOrEmpty -Because 'The prefix is not defined'
 		}
 	}
 
