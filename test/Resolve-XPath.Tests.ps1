191a0aeb01cb568219d3f9dda8de9ab9de232e21
Brian Lalonde <brianary@gmail.com>
2026-08-02 17:09:25 -0700

    ✅ Add tests for Resolve-XPath
    - resolves #16
    - resolves #17

--- test/Resolve-XPath.Tests.ps1
+++ test/Resolve-XPath.Tests.ps1
@@ -1,26 +1,30 @@
 ﻿<#
 .SYNOPSIS
-Tests Returns the XPath of the location of an XML node.
+Tests returning the XPath of the location of an XML node.
 #>
 
-return #TODO: finish tests
 if(!(&"$PSScriptRoot/../scripts/Test-RelevantTest.ps1")) {return}
 BeforeAll {
 	Set-StrictMode -Version Latest
 	&"$PSScriptRoot/../scripts/Import-ThisModule.ps1"
+	$datadir = Join-Path $PSScriptRoot 'data'
 }
 Describe 'Resolve-XPath' -Tag Resolve-XPath,Resolve,XPath {
-	Context 'Returns the XPath of the location of an XML node.' -Tag Example {
-		It "EXAMPLE 1" -Skip {
-			'<a><b c="value"/></a>' |Select-Xml //@c |Resolve-XPath |Should -BeExactly @"
-/a/b/@c
-"@
+	Context 'Returns the XPath of the location of an XML node.' {
+		It "Should return a simple XPath" {
+			$xpath = '<a><b c="value"/></a>' |Select-Xml //@c |Resolve-XPath
+			$xpath.XPath |Should -BeExactly '/a/b/@c'
+			$xpath.Namespace.Count |Should -BeExactly 0
 		}
-		It "EXAMPLE 2" -Skip {
-			'<a>one<!-- two -->three</a>' |Select-Xml '//text()' |Resolve-XPath |Should -BeExactly @"
-/a/text()[1]
-/a/text()[2]
-"@
+		It "Should return multiple XPaths with a namspace" {
+			$xpaths = Select-Xml '/xhtml:*/text()' -Path (Join-Path $datadir test.xhtml) `
+				-Namespace @{'xhtml'='http://www.w3.org/1999/xhtml'} |Resolve-XPath
+			$xpaths[0].XPath |Should -BeExactly '/xhtml:div/text()[1]'
+			$xpaths[0].Namespace.Count |Should -BeExactly 1
+			$xpaths[0].Namespace['xhtml'] |Should -BeExactly http://www.w3.org/1999/xhtml
+			$xpaths[1].XPath |Should -BeExactly '/xhtml:div/text()[2]'
+			$xpaths[1].Namespace.Count |Should -BeExactly 1
+			$xpaths[1].Namespace['xhtml'] |Should -BeExactly http://www.w3.org/1999/xhtml
 		}
 	}
 
--- test/Resolve-XmlSchemaLocation.Tests.ps1
+++ test/Resolve-XmlSchemaLocation.Tests.ps1
@@ -13,28 +13,23 @@ Describe 'Resolve-XmlSchemaLocation' -Tag Resolve-XmlSchemaLocation,Resolve,XmlS
 	Context 'Gets the namespaces and their URIs and URLs from a document.' {
 		It "Finds the schemata within the given XML document namespaces" {
 			$schemata = @{}
-			Resolve-XmlSchemaLocation (Join-Path $datadir xslt-schemata.xslt) |
+			Resolve-XmlSchemaLocation (Join-Path $datadir test.xhtml) |
 				ForEach-Object {$schemata[$_.Alias] = $_}
-			$schemata['xml'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata['xml'].Path |Split-Path -Leaf |Should -BeExactly test.xhtml
 			$schemata['xml'].Alias |Should -BeExactly xml
 			$schemata['xml'].Urn |Should -BeExactly http://www.w3.org/XML/1998/namespace
 			$schemata['xml'].Url |Should -BeNullOrEmpty
-			$schemata['xsi'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata['xml'].Node.Name |Should -BeExactly div
+			$schemata['xsi'].Path |Split-Path -Leaf |Should -BeExactly test.xhtml
 			$schemata['xsi'].Alias |Should -BeExactly xsi
 			$schemata['xsi'].Urn |Should -BeExactly http://www.w3.org/2001/XMLSchema-instance
 			$schemata['xsi'].Url |Should -BeNullOrEmpty
-			$schemata['fn'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
-			$schemata['fn'].Alias |Should -BeExactly fn
-			$schemata['fn'].Urn |Should -BeExactly http://www.w3.org/2005/xpath-functions
-			$schemata['fn'].Url |Should -BeNullOrEmpty
-			$schemata['xsl'].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
-			$schemata['xsl'].Alias |Should -BeExactly xsl
-			$schemata['xsl'].Urn |Should -BeExactly http://www.w3.org/1999/XSL/Transform
-			$schemata['xsl'].Url |Should -BeExactly http://www.w3.org/1999/XSL/Transform.xsd
-			$schemata[''].Path |Split-Path -Leaf |Should -BeExactly xslt-schemata.xslt
+			$schemata['xsi'].Node.Name |Should -BeExactly div
+			$schemata[''].Path |Split-Path -Leaf |Should -BeExactly test.xhtml
 			$schemata[''].Alias |Should -BeNullOrEmpty
 			$schemata[''].Urn |Should -BeExactly http://www.w3.org/1999/xhtml
 			$schemata[''].Url |Should -BeExactly http://www.w3.org/1999/xhtml.xsd
+			$schemata[''].Node.Name |Should -BeExactly div
 		}
 	}
 }
